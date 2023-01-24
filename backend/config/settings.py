de35e5d6825218087b6aa5bae7caffbbb9e74f9a
vaduska <vadimsalapugin@gmail.com>
2023-01-24 22:39:18 +0500

    Added PostgreSQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -75,11 +75,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_NAME"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+    }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     },
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
---
+++ db/README.md
@@ -0,0 +1,6 @@
+<div class="container">
+    <img src="https://media.tenor.com/yfRbDajBsC8AAAAC/spongebob-database.gif"
+    alt="this slowpoke moves"  
+    height="500"
+    width="800" />
+</div>
