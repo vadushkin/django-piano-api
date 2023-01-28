24c4889d033b5e7c033ca89e759420c8f38156d9
vaduska <vadimsalapugin@gmail.com>
2023-01-28 21:36:50 +0500

    Added Tests for Categories

--- backend/sheets/tests.py
+++ backend/sheets/tests.py
@@ -24,11 +24,6 @@ class TestSheets(APITestCase):
             "password": "oak",
         }
 
-        log_user2 = {
-            "email": "oak2@oak.com",
-            "password": "oak2",
-        }
-
         # Register
         self.client.post(
             self.register_url,
@@ -202,7 +197,7 @@ class TestSheets(APITestCase):
         )
         # check good error
         self.assertEqual(response.status_code, 200)
-        print(response.data)
+
         # check that we got data
         self.assertTrue(len(response.data) > 0)
 
@@ -237,55 +232,306 @@ class TestSheets(APITestCase):
 class TestCategories(APITestCase):
     categories_url = "/api/v1/categories/"
 
-    def test_create(self):
-        pass
+    register_url = "/users/register/"
+    login_url = "/users/login/"
 
-    def test_get_one(self):
-        pass
+    def _prepare_for_tests(self):
+        reg_user1 = {
+            "name": "fir",
+            "email": "fir@fir.com",
+            "password": "fir",
+        }
 
-    def test_get_many(self):
-        pass
+        reg_user2 = {
+            "name": "fir2",
+            "email": "fir2@fir.com",
+            "password": "fir2",
+        }
 
-    def test_update(self):
-        pass
+        log_user1 = {
+            "email": "fir@fir.com",
+            "password": "fir",
+        }
 
-    def test_logout_delete(self):
-        pass
+        # Register
+        self.client.post(
+            self.register_url,
+            data=reg_user1,
+        )
+        self.client.post(
+            self.register_url,
+            data=reg_user2,
+        )
+
+        # Login
+        self.client.post(
+            self.login_url,
+            data=log_user1,
+        )
+
+    def test_crud(self):
+        ################################################################################
+        # Create #
+        ################################################################################
+
+        # preparing
+        self._prepare_for_tests()
+
+        # test payloads
+        test_payload1 = {
+            "name": "Bagatelle 25",
+            "slug": "bagatelle-25",
+            "user": 1,
+        }
+        test_payload2 = {
+            "name": "Etud op10 12",
+            "slug": "etud-op10-12",
+            "user": 1,
+        }
+        test_payload3 = {
+            "name": "Preludiya op32 2",
+            "slug": "preludiya-op32-2",
+            "user": 2,
+        }
+
+        # response from server
+        response = self.client.post(
+            self.categories_url,
+            data=test_payload1,
+            format="multipart",
+        )
+        # bad/good?
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data.get("name"), "Bagatelle 25")
+
+        # response from server
+        response = self.client.post(
+            self.categories_url,
+            data=test_payload2,
+            format="multipart",
+        )
+        # nice/cool/awesome?
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data.get("name"), "Etud op10 12")
+
+        response = self.client.post(
+            self.categories_url,
+            data=test_payload3,
+            format="multipart",
+        )
+        # very nice
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data.get("name"), "Preludiya op32 2")
+
+        ################################################################################
+        # Patch #
+        ################################################################################
+
+        test_update_data1 = {
+            "name": "Spruce",
+        }
+
+        test_update_data2 = {
+            "name": "Fir",
+        }
+
+        # response from server
+        response = self.client.patch(
+            self.categories_url + "1/",
+            data=test_update_data1,
+        )
+        # check good error
+        self.assertEqual(response.status_code, 200)
+        # check that name was changed
+        self.assertEqual(response.data.get("name"), "Spruce")
+        # check that user is not changed
+        self.assertEqual(response.data.get("slug"), "bagatelle-25")
+
+        # response from server
+        response = self.client.patch(
+            self.categories_url + "2/",
+            data=test_update_data2,
+        )
+        # check good error
+        self.assertEqual(response.status_code, 200)
+        # check that name was changed
+        self.assertEqual(response.data.get("name"), "Fir")
+        # check that user is not changed
+        self.assertEqual(response.data.get("slug"), "etud-op10-12")
+
+        ################################################################################
+        # Put #
+        ################################################################################
+
+        test_update_data1 = {
+            "name": "Herringbone",
+            "slug": "cool",
+            "user": 1,
+        }
+        test_update_data2 = {
+            "name": "Christmas Tree",
+            "slug": "nice",
+            "user": 1,
+        }
+
+        # response from server
+        response = self.client.put(
+            self.categories_url + "1/",
+            data=test_update_data1,
+        )
+
+        # check good error
+        self.assertEqual(response.status_code, 200)
+        # check that name and user were changed
+        self.assertEqual(response.data.get("name"), "Herringbone")
+        self.assertEqual(response.data.get("slug"), "cool")
+
+        # response from server
+        response = self.client.put(
+            self.categories_url + "2/",
+            data=test_update_data2,
+        )
+        # check good error
+        self.assertEqual(response.status_code, 200)
+        # check that name and user were changed
+        self.assertEqual(response.data.get("name"), "Christmas Tree")
+        self.assertEqual(response.data.get("slug"), "nice")
+
+        ################################################################################
+        # Get #
+        ################################################################################
+
+        # response from server
+        response = self.client.get(
+            self.categories_url,
+        )
+        # check good error
+        self.assertEqual(response.status_code, 200)
+
+        # check that we got data
+        self.assertTrue(len(response.data) > 0)
+
+        # response
+        response = self.client.get(
+            self.categories_url + "2/",
+        )
+        # check bad error
+        self.assertEqual(response.status_code, 200)
+        # check that we got the Herringbone
+        self.assertEqual(response.data.get("name"), "Christmas Tree")
+
+        ################################################################################
+        # Delete #
+        ################################################################################
+
+        # response from server
+        response = self.client.delete(
+            self.categories_url + "1/",
+        )
+        # alright?
+        self.assertEqual(response.status_code, 204)
+
+        # response from server
+        response = self.client.delete(
+            self.categories_url + "2/",
+        )
+        # nice.
+        self.assertEqual(response.status_code, 204)
 
 
 class TestTags(APITestCase):
     tags_url = "/api/v1/tags/"
 
-    def test_create(self):
-        pass
+    register_url = "/users/register/"
+    login_url = "/users/login/"
 
-    def test_get_one(self):
-        pass
+    def _prepare_for_tests(self):
+        reg_user1 = {
+            "name": "pine",
+            "email": "pine@pine.com",
+            "password": "pine",
+        }
 
-    def test_get_many(self):
-        pass
+        reg_user2 = {
+            "name": "pine2",
+            "email": "pine2@pine.com",
+            "password": "pine2",
+        }
 
-    def test_update(self):
-        pass
+        log_user1 = {
+            "email": "pine@pine.com",
+            "password": "pine",
+        }
 
-    def test_logout_delete(self):
-        pass
+        # Register
+        self.client.post(
+            self.register_url,
+            data=reg_user1,
+        )
+        self.client.post(
+            self.register_url,
+            data=reg_user2,
+        )
+
+        # Login
+        self.client.post(
+            self.login_url,
+            data=log_user1,
+        )
+
+    def test_crud(self):
+        ################################################################################
+        # Create #
+        ################################################################################
+
+        # preparing
+        self._prepare_for_tests()
 
 
 class TestAuthors(APITestCase):
     authors_url = "/api/v1/authors/"
 
-    def test_create(self):
-        pass
+    register_url = "/users/register/"
+    login_url = "/users/login/"
 
-    def test_get_one(self):
-        pass
+    def _prepare_for_tests(self):
+        reg_user1 = {
+            "name": "maple",
+            "email": "maple@maple.com",
+            "password": "maple",
+        }
 
-    def test_get_many(self):
-        pass
+        reg_user2 = {
+            "name": "maple2",
+            "email": "maple2@maple.com",
+            "password": "maple2",
+        }
 
-    def test_update(self):
-        pass
+        log_user1 = {
+            "email": "maple@maple.com",
+            "password": "maple",
+        }
 
-    def test_logout_delete(self):
-        pass
+        # Register
+        self.client.post(
+            self.register_url,
+            data=reg_user1,
+        )
+        self.client.post(
+            self.register_url,
+            data=reg_user2,
+        )
+
+        # Login
+        self.client.post(
+            self.login_url,
+            data=log_user1,
+        )
+
+    def test_crud(self):
+        ################################################################################
+        # Create #
+        ################################################################################
+
+        # preparing
+        self._prepare_for_tests()
